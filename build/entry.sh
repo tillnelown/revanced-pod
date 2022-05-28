15b64300ee359eadb3a1f80ffa5cdc707cc373f5
Till Nelown <hllpd200@gmail.com>
2022-05-28 20:34:21 +0200

    Remove broken upstream patches and add more Troubleshooting options

--- build/entry.sh
+++ build/entry.sh
@@ -8,8 +8,11 @@ NORMAL=$(tput sgr0)
 ERROR_COLOR=$(tput setaf 1)
 UNDERLINE=$(tput smul)
 
+if [ ! -n "${PATCHES}" ]; then
+	# All patches that will be applied, the patcher will check if the patch is made for the apk before thoug so both youtube and youtube music patches are in this list.
+	PATCHES="codecs-unlock exclusive-audio-playback tasteBuilder-remover upgrade-button-remover background-play home-promo-ads video-ads seekbar-tapping amoled disable-create-button minimized-playback old-quality-layout shorts-button integrations locale-config-fix"
+fi
 
-set -e
 
 # Function that handles compiling the repositories
 function BuildIt {
@@ -82,13 +85,14 @@ chmod ug+x revanced-*/gradlew
 for buildingrepo in $buildingrepos; do
         BuildIt
         if [ "$RETURN_CODE" -ne "0" ]; then
-                echo "${ERROR_COLOR}$buildingrepo build failed with exit code $RETURN_CODE${NORMAL}"
+                echo "${ERROR_COLOR}buildingrepo build failed with exit code $RETURN_CODE${NORMAL}"
                 abort=1
         fi
 done
 
 if [[ abort -eq 1 ]]; then
-        exit 1
+	echo "${ERROR_COLOR}Compiling failed, dropping to shell...${NORMAL}"
+	exec bash
 fi
 
 
@@ -108,23 +112,25 @@ cd /root/build
 cp /volume/youtube.apk .
 
 # Patching
+if [ ! -n "${EXIT_TO_BASH}" ]; then
+	if [ $adb -eq 1 ]; then
+        	echo "${UNDERLINE}Make sure the app + version you are patching is installed${NORMAL}"
+	        sleep 1
+        	java -jar revanced-cli-all.jar -a youtube.apk -d $(adb devices 2>/dev/null | grep -oP '^[^ ]*(?= *device)') -m revanced-integrations.apk -o revanced.apk -p revanced-patches.jar -rt cache $PATCHES
+	else
+        	java -jar revanced-cli-all.jar -a youtube.apk -m revanced-integrations.apk -o revanced.apk -p revanced-patches.jar -rt cache $PATCHES
+	fi
 
-if [ $adb -eq 1 ]; then
-        echo "${UNDERLINE}Make sure the app + version you are patching is installed${NORMAL}"
-        sleep 1
-        java -jar revanced-cli-all.jar -a youtube.apk -d $(adb devices 2>/dev/null | grep -oP '^[^ ]*(?= *device)') -m revanced-integrations.apk -o revanced.apk -p revanced-patches.jar -rt cache
+	mkdir -p /volume/jars
+	cp -f revanced-cli-all.jar revanced-integrations.apk revanced-patches.jar /volume/jars/
+
+	# revanced-cli doesn't give the correct exit code, see if revanced is created instead and check for success that way
+	cp -f revanced.apk /volume/revanced.apk
+	if [ "$RETURN_CODE" -eq "0" ]; then
+		exit 0
+	else
+		exit 1
+	fi
 else
-        java -jar revanced-cli-all.jar -a youtube.apk -m revanced-integrations.apk -o revanced.apk -p revanced-patches.jar -rt cache
-fi
-
-mkdir -p /volume/jars
-cp -f revanced-cli-all.jar revanced-integrations.apk revanced-patches.jar /volume/jars/
-
-# revanced-cli doesn't give the correct exit code, see if revanced is created instead and check for success that way
-cp -f revanced.apk /volume/revanced.apk
-
-if [ "$RETURN_CODE" -eq "0" ]; then
-	exit 0
-else
-	exit 1
+	exec bash
 fi
