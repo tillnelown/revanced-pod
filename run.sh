3eb83b9de85bf162f40788e4bd6dba14703ceafb
Till Nelown <hllpd200@gmail.com>
2022-05-28 20:55:40 +0200

    Improve look of gradlew and make bash work

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 if [ ! -d cache ]; then
 	mkdir cache
 fi
-exec podman run --net=host --rm -v ~/Android/:/android -v $(pwd)/shared:/volume -v $(pwd)/cache:/root/.gradle revanced:latest
+exec podman run --net=host -it --rm -v ~/Android/:/android -v $(pwd)/shared:/volume -v $(pwd)/cache:/root/.gradle revanced:latest
 
